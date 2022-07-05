dc7f5ac2e1ef104212ab2de374fb9afb2c4e3520
Omar-Eidaros <96912381+Omar-Eidaros@users.noreply.github.com>
2022-07-05 21:39:14 +0200

    First Deploy

--- WebApp/src/main/java/com/insights/verification/TwilioClass.java
+++ WebApp/src/main/java/com/insights/verification/TwilioClass.java
@@ -7,7 +7,7 @@ import com.twilio.type.PhoneNumber;
 public class TwilioClass {
 
     private static final String ACCOUNT_SID = "ACc73541e70d0f014ad651ed5b2830ab6f";
-    private static final String AUTH_TOKEN = "a936654c22ae735273b587ea0ed9c9c8";
+    private static final String AUTH_TOKEN = "f45f3bdfe65e789df56878fb289b2372";
     private static final String PHONE_NUM = "+19897955893";
 
     public static void createMessage(String msisdn, String verifyCode){
