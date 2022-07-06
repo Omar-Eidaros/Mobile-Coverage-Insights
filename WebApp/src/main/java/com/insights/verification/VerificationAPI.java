9760d60de42491524ffbcb359331d12c752be15d
Omar-Eidaros <96912381+Omar-Eidaros@users.noreply.github.com>
2022-07-06 03:59:29 +0200

    Modify Some Pugs 2

---
+++ WebApp/src/main/java/com/insights/verification/VerificationAPI.java
@@ -0,0 +1,43 @@
+package com.insights.verification;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import com.insights.verification.*;
+
+@Path("/verification")
+public class VerificationAPI {
+
+    @POST
+    @Path("/sendCode")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response sendCode(Verification verification){
+        String result = null;
+
+        int state = verification.sendCode(verification);
+
+        if (state == 1){
+            result = "Verification Code have been sent";
+        } else {
+            result ="Verification Code not received yet";
+        }
+        return Response.status(200).entity(state).build();
+    }
+
+    @GET
+    @Path("isCodeValid")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response checkCode(Verification verification){
+
+        String result = verification.checkCode(verification);
+
+        if (result.equals("1")){
+            System.out.println("Verification Code is Valid");
+            return Response.status(200).entity(result).build();
+        } else {
+            System.out.println("Verification Code is not exist");
+            return Response.status(Response.Status.BAD_REQUEST).build();
+        }
+    }
+}
